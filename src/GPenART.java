6716fbc95a254e8c3971e25fee16b2c1ad2b3b8c
Sn0w <garrettbeasley@icloud.com>
2014-09-24 17:56:01 -0700

    Flag Design

--- src/GPenART.java
+++ src/GPenART.java
@@ -16,15 +16,83 @@ public class GPenART extends GraphicsProgram
 {
 	public void run()
 	{
+		//Variabiles for the program
+		int startX = 0;
+		int startY = 0;
+		int windowX = 1336;
+		int windowY = 768;
+		int flagsep = 70;
+		
+		 setSize(windowX, windowY);
+				
 		//Creates the Pen at location 25, 25 and addes to the canvas.
 		GPen pen = new GPen();
 		add(pen);
-		pen.setLocation(25, 25);
+		pen.setLocation(startX, startY);
 		pen.showPen();
 		
-		//Start the drawing
-		pen.drawLine(50, 50);
-		pen.hidePen();
+		//Start drawing the edges/layout of the flag
+		pen.drawLine(0, 768);
+		pen.drawLine(1336, 0);
+		pen.drawLine(0, -768);
+		pen.drawLine(-1336, 0);
+		
+		//Strip #1
+		pen.setLocation(startX, (windowY-flagsep));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #2
+		pen.setLocation(startX, (windowY-(flagsep*2)));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #3
+		pen.setLocation(startX, (windowY-(flagsep*3)));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #4
+		pen.setLocation(startX, (windowY-(flagsep*4)));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #5
+		pen.setLocation(startX, (windowY-(flagsep*5)));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #6
+		pen.setLocation(startX, (windowY-(flagsep*6)));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #7
+		pen.setLocation(startX+450, (windowY-(flagsep*7)));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #8
+		pen.setLocation(startX+450, (windowY-(flagsep*8)));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #9
+		pen.setLocation(startX+450, (windowY-(flagsep*9)));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #10
+		pen.setLocation(startX+450, (windowY-(flagsep*10)));
+		pen.drawLine(windowX, 0);
+		
+		//Strip #11
+		pen.setLocation(startX+450, (windowY-(flagsep*11)));
+		pen.drawLine(windowX, 0);
+		
+		//StarBox
+		pen.setLocation(startX+450, startY);
+		pen.drawLine(startX, 348);
+		
+		
+		
+		
+		
+		
+		System.out.println(windowY-(flagsep*6));		
+		
+		//pen.hidePen();
 		
 	}
 
